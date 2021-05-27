f752f797641f6ce5d73c6af4d4db86deec6d3202
Alban Rochet <albanrochet@gmail.com>
2021-05-27 13:00:19 +0200

    Session 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  titre: string = "Formation OCC";
+  listTaches: string[] = ["Ranger ma chambre", "tache 2", "tache 3", "tache 4"];//List de mes taches
 
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  ajouterTache() {
+    //Ajout d'un nouvelle élément dans le tableau listTaches
+    this.listTaches.push("Nouvelle Tache " + (this.listTaches.length + 1));
+  }
 }
